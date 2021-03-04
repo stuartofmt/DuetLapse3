e14d2b957da67b2d67aea0a269de3da46d8dbdca
stuartofmt <stuartofmt@users.noreply.github.com>
2021-03-04 15:29:37 -0700

    3.4.0

--- startDuetLapse3.py
+++ startDuetLapse3.py
@@ -8,9 +8,8 @@ Simple HTTP server for starting and stopping DuetLapse3
 # Developed on WSL with Debian Buster. Tested on Raspberry pi, Windows 10 and WSL. SHOULD work on most other linux distributions. 
 """
 global startDuetLapse3Version
-startDuetLapse3Version = '3.3.0'
+startDuetLapse3Version = '3.4.0'
 import argparse
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 import sys
 import threading
@@ -23,6 +22,7 @@ import time
 import platform
 import urllib
 import html
+import requests
 
 class whitelistParser(argparse.ArgumentParser):
     def exit(self, status=0, message=None):
@@ -43,16 +43,43 @@ def init():
     host = args['host'][0]
     port = args['port'][0]
     defaultargs = args['args'][0]
+    
+###########################
+# make Web calls
+###########################
+def blindUrlCall(url, timelimit):  #Fire and forget - assumed to be successful
+    threading.Thread(target=urlCall, args=(url, timelimit)).start()
 
+def urlCall(url,timelimit):    
+            
+    loop = 0
+    limit = 2  #Started at 2 - seems good enough to catch transients
+    while (loop < limit):    
+        try:
+            #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}           
+            r = requests.get(url, timeout=timelimit)
+            break
+        except requests.ConnectionError as e:
+            print('There was a network failure: '+str(e))
+            loop += 1
+        except requests.exceptions.Timeout as e:
+            print('There was a timeout failure: '+str(e))
+            loop += 1
+        time.sleep(1)            
+            
+    return True
 
 ###########################
 # Integral Web Server
 ###########################
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 from urllib.parse import urlparse, parse_qs
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory= basedir, **kwargs)   
+    
     
     def _set_headers(self):
         self.send_response(200)
@@ -73,7 +100,7 @@ class MyHandler(BaseHTTPRequestHandler):
         pidlist = []
         localtime = time.strftime('%A - %H:%M',time.localtime())
         #portlist = []
-        runninginstances, pidlist = getRunningInstances(thisinstance, referer)
+        runninginstances, pidlist = getRunningInstances(thisinstance, refererip)
         thisrunninginstance = getThisInstance(thisinstancepid)
     
         header =    ('startDuetLapse3 Version '+startDuetLapse3Version+'<br>'
@@ -113,6 +140,12 @@ class MyHandler(BaseHTTPRequestHandler):
                     '<input type="submit" value="Shutdown" style="background-color:red"/>'
                     '</form>'
                     '</div>'
+                    '<div class="divider">'
+                    '<form target="_blank" action="http://'+referer+'">'
+                    '<input type="hidden" name="files" value= '+basedir+' />'
+                    '<input type="submit" value="Files" style="background-color:green"/>'                    
+                    '</form>'
+                    '</div>' 
                     '<style type="text/css">'
                     '{'
                     'position:relative;'
@@ -132,28 +165,35 @@ class MyHandler(BaseHTTPRequestHandler):
                     '</style>'
                     )        
         return header , status , buttons;
-        
+
+    
     def do_GET(self):
-        self._set_headers()
-        global referer
-        referer = self.headers['Host']       
-        #Update main content
-        header, status, buttons = self.update_content()
-
-        path = self.path
-
-        if ('favicon.ico' in path):
+        if ('favicon.ico' in self.path):
             return
-            
+
         query_components = parse_qs(urlparse(self.path).query)
+
+        if ((query_components and not query_components.get('command')) or (not query_components and self.path != '/')):
+
+            super().do_GET()        
+            return    
+            
+        global referer, refererip
+        referer = self.headers['Host']
+        split_referer = referer.split(":", 1)
+        refererip = split_referer[0]            
+        #Update main content
+        header, status, buttons = self.update_content()                      
+        self._set_headers()
+
             
         command = ''
         if(query_components.get('command')):
-            command = query_components['command'][0]
-
-
-                
-
+            #Update main content
+            header, status, buttons = self.update_content()
+           
+            command = query_components['command'][0]        
+        
             if(query_components.get('nohup')):
                 nohup = query_components['nohup'][0]
             else:
@@ -217,7 +257,8 @@ class MyHandler(BaseHTTPRequestHandler):
                     setstart =  ('<h3>'
                                 'Attempting to start DuetLapse3 with following options:<br>'
                                 '<pre>'
-                                +cmd+
+                                +cmd+'<br><br>'
+                                'Press Status to refresh'
                                 '</pre>'
                                 '</h3>'
                                 )                        
@@ -226,11 +267,11 @@ class MyHandler(BaseHTTPRequestHandler):
                     self.wfile.write(self._html( header+status+buttons+setstart))
                     
                 else:
-                    print('C: '+ defaultargs)
                     getstart =  ('<div>'                                    
                                 '<form action="http://'+referer+'">'
                                 '<input type="hidden" name="command" value="start" />'
-                                '<input type="text" id="args" name="args" value="'+defaultargs+'" size="200"/>'                                    
+                                '<input type="text" id="args" name="args" value="'+defaultargs+'" size="200"/>'
+                                '<br>'
                                 '<input class = "pad30" type="submit" value="Start" style="background-color:orange"/>'
                                 '</form>'
                                 '</div>'
@@ -239,34 +280,52 @@ class MyHandler(BaseHTTPRequestHandler):
                                                 
             elif (command == 'terminate'):
                 if (pids != ''):
+                                        
                     if (pids == 'all'):
-                        for pid in pidlist:
-                            try:
-                                os.kill(pid, 2)
-                            except:
-                                pass
                         pidmsg = 'All running instances'
                     else:
-                        pid = int(pids)
-                        try:
-                            os.kill(pid, 2)
-                            pidmsg = 'The instance with pid = '+pids
-                        except:
-                            pidmsg = 'There was no instance with pid = '+pids
+                        pidmsg = 'For instances with process id: '+str(pids)
+                        
+                    for pid in pidlist:
+                        thispid = pid[0]
+                        thisport = pid[1]
+                        if (pids == 'all' or pids == str(thispid)):
+                            if (thisport == 0):
+                                try:
+                                    os.kill(thispid, 2)
+                                except:
+                                    pass
+                            else:
+                                URL = 'http://'+refererip+':'+str(thisport)+'/?command=terminate'
+                                #blindUrlCall(URL, 5)
+                                urlCall(URL, 5)
+
                     setterminate =   ('<h3>'
-                                     'Terminating the following DuetLapse3 Instances.<br>'
-                                     '<pre>'
-                                     +pidmsg+'<br>'
-                                     'Note that some instances may take several minutes to shut down'
-                                     '</pre>'
-                                     '</h3>'
-                                     )
+                                      'Terminating the following DuetLapse3 Instances.<br>'
+                                      '<pre>'
+                                      +pidmsg+'<br>'
+                                      'Note that some instances may take several minutes to shut down<br><br>'
+                                      'Press Status to refresh'
+                                      '</pre>'
+                                      '</h3>'
+                                      )
                     
                     self.wfile.write(self._html( header+status+buttons+setterminate))
 
                 else:
-                    getterminate =  ('<div>'                                    
+                    if win:
+                        winnote = ('<h3><pre>'
+                                   'NOTE: Only instances with http ports will terminate gracefully<br>'
+                                   'All others will just shutdown (no video created)<br>'
+                                   'This is a windows limitation'
+                                   '</pre></h3>'
+                                   )
+                    else:
+                        winnote = ''
+                        
+                    getterminate =  ('<div>'
                                     '<form action="http://'+referer+'">'
+                                    +winnote+'<br>'
                                     '<input type="hidden" name="command" value="terminate" />'
                                     '<input type="text" id="pid" name="pids" value="all" size="6"/>'                                    
                                     '<input class = "pad30" type="submit" value="Terminate" style="background-color:yellow"/>'
@@ -277,14 +336,14 @@ class MyHandler(BaseHTTPRequestHandler):
 
             elif (command == 'shutdown'):
                 localtime = time.strftime('%A - %H:%M',time.localtime())
-                self.wfile.write(self._html('Shutting Down startDuetLapse3.<br><br>'
-                                            'At:  '+localtime+'<br><br>'
-                                            '<h3>'
-                                            'Any instances of DuetLapse3 will continue to run'
-                                            '</h3>'
-                                            ))
-                print('!!!!!! Stopped by http request !!!!!!')
-                terminate()
+                shutdown = ('<h1><pre>'
+                            'Shutting Down startDuetLapse3.<br>'
+                            'Any instances of DuetLapse3 will continue to run'
+                            '</pre></h1>'
+                            )
+                self.wfile.write(self._html( header+status+buttons+shutdown))                            
+                print('!!!!!! Shutdown by http request !!!!!!')
+                shut_down()
 
             else:
                 self.wfile.write(self._html('Illegal value for command=<br>'
@@ -293,15 +352,18 @@ class MyHandler(BaseHTTPRequestHandler):
                                             ))
                        
             return
-
-        refreshing = ('<h3><pre>'
-                      'Status will update every 60 seconds'
-                      '</pre></h3>'
-                      )
-        self.wfile.write(self._refresh(header+status+buttons+refreshing))
-
+       
+        else:
         
-        return
+            refreshing = ('<h3><pre>'
+                          'Status will update every 60 seconds'
+                          '</pre></h3>'
+                          )
+                  
+            self.wfile.write(self._refresh(header+status+buttons+refreshing))
+
+            
+            return
 
     def log_request(self, code=None, size=None):
         pass
@@ -311,7 +373,7 @@ class MyHandler(BaseHTTPRequestHandler):
     
 def createHttpListener():
     global listener
-    listener = HTTPServer((host, port), MyHandler)
+    listener = ThreadingHTTPServer((host, port), MyHandler)
     listener.serve_forever()
     
 def closeHttpListener():
@@ -319,7 +381,7 @@ def closeHttpListener():
     print('!!!!! Stop requested by http listener !!!!!')
     listener.shutdown()
     listener.server_close()
-    print('Terminated')
+    print('Shutdown')
     sys.exit(0)
     
 def getOperatingSystem():
@@ -347,9 +409,7 @@ def getThisInstance(thisinstancepid):
        
     return  thisrunning  
         
-def getRunningInstances(thisinstance, referer):
-    split_referer = referer.split(":", 1)
-    refererip = split_referer[0]  
+def getRunningInstances(thisinstance, refererip):
     running = ''
     pidlist = []
     for p in psutil.process_iter():
@@ -362,6 +422,7 @@ def getRunningInstances(thisinstance, referer):
                 port = cmdline[index]
             except ValueError:
                  port = 0
+                 
             #clean up the appearance
             cmdlinestr = str(p.cmdline())
             cmdlinestr = cmdlinestr.replace('[','')
@@ -369,7 +430,7 @@ def getRunningInstances(thisinstance, referer):
             cmdlinestr = cmdlinestr.replace(',','')
             cmdlinestr = cmdlinestr.replace("'",'')
             cmdlinestr = cmdlinestr.replace('  ','')
-            pidlist.append(p.pid)
+            pidlist.append((p.pid,port))
             pid = str(p.pid)
             if (port == 0):
                 running = running+('Process id:  '+pid+'<br>'
@@ -389,15 +450,16 @@ def getRunningInstances(thisinstance, referer):
                   '</pre>'
                   )
     else:
-        running = 'None'  
+        running = 'None'   
     return running, pidlist;
         
-def terminate():
+def shut_down():
     try:    #this should close this thread
         httpthread.join(10)
     except:
         pass   
     os.kill(thisinstancepid, 9)
+    
     
 #Allows process running in background or foreground to be gracefully
 # shutdown with SIGINT (kill -2 <pid> also handles KeyboardInterupt
@@ -405,7 +467,7 @@ import signal
 
 def quit_gracefully(*args):
     print('!!!!!! Stopped by SIGINT or CTL+C  !!!!!!')
-    terminate()
+    shut_down()
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit_gracefully)    
@@ -415,9 +477,10 @@ Main Program
 """
 if __name__ == "__main__":
    
-    global thisinstance, thisinstancepid
+    global thisinstance, thisinstancepid,basedir
     getOperatingSystem()            #some commands are os specific
     thisinstance = os.path.basename(__file__)
+    basedir = os.path.dirname(os.path.realpath(__file__))
     if not win:
         thisinstance = './'+thisinstance
     _ = checkInstances(thisinstance,'single')    #There can only be one instance running
@@ -433,8 +496,8 @@ if __name__ == "__main__":
                 print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                 sys.exit(2)
                 
-            httpthread = threading.Thread(target=createHttpListener, args=())
-            httpthread.start()
+            httpthread = threading.Thread(target=createHttpListener, args=()).start()
+            #httpthread.start()
             print('***** Started http listener *****')
             
         except KeyboardInterrupt:
