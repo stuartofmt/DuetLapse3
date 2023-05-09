f9bfd321842d5d8e6bfe0c02ce9530b9887ed300
stuartofmt <stuartofmt@users.noreply.github.com>
2023-05-09 09:47:09 -0600

    5.2.4

--- plugin/3.5.0.beta.3/src/DuetLapse3.vue
+++ plugin/3.5.0.beta.3/src/DuetLapse3.vue
@@ -1,4 +1,8 @@
-//Thanks to @MintyTrebor for all the help in getting this working
+<--!
+Displays an iFrame that is linked to the DuetLapse3 html Display
+Polls the status of the sbcPlugin and terminates the DWC plugin if the sbcPlugin is terminated
+Thanks to @MintyTrebor for all the help in getting this working
+-->
 <style scoped>
 	.iframe-container {
 		position: relative;
@@ -13,7 +17,7 @@
  
 <template>
 		<div class="iframe_container">
-			<iframe :src= "myurl" width="100%" :height="tmpHeight" frameborder="0">
+			<iframe id="myiframe" :src= "myurl" width="100%" :height="tmpHeight" frameborder="0">
 				<span>Your browser does not support iFrames</span>
 			</iframe>
 		</div>
@@ -23,23 +27,26 @@
 import Path from '@/utils/path';
 import store from "@/store";
 import { DisconnectedError, OperationCancelledError } from "@/utils/errors";
- 
-const version = '0.0.1';
-const configFile = './DuetLapse3/DuetLapse3.config';
- 
+
+// <!-- Do not change
+const pluginName = 'DuetLapse3';
+const configFile = './' + pluginName + '/' + pluginName + '.config';
+// -->
+window.onmessage = function(event){
+    if (event.data == 'reply') {
+        console('Reply received!');
+    }
+};
 export default {
 	data() { 
 		return{
-			version: version,
 			myurl: '',
 			tmpHeight: "400px",
-			fileContent: ""
-		};
+		}
 	},
 	methods: {		
         //Modified file load from @mintyTrebor 
 		async loadSettingsFromFile() {
-			//Load the DuetLapse3.config file
 			try {
 				const setFileName = Path.combine(this.systemDirectory, configFile);
 				const response = await store.dispatch("machine/download", { filename: setFileName, type: 'text', showSuccess: false, showError: false});
@@ -47,11 +54,11 @@ export default {
 				//get the ip address
 				let pattern = /-duet.*(?:$|\n)/i;
 				let match = this.fileContent.match(pattern).toString();
-				this.myurl = "http://"+ match.replace('-duet', '').trim();
+				this.topurl = "http://"+ match.replace('-duet', '').trim();
 				// get the port
 				pattern = /-port.*(?:$|\n)/i;
 				match = this.fileContent.match(pattern).toString();
-				this.myurl = this.myurl + ":" + match.replace('-port', '').trim();
+				this.myurl = this.topurl + ":" + match.replace('-port', '').trim();
 				console.log('DuetLapse url is ' + this.myurl);
  
 			} catch (e) {
@@ -61,6 +68,7 @@ export default {
 				}
 			}
 		},
+		// Set the screen height - from @MintyTrebor
 		getAvailScreenHeight(){
 		let tmpHeight = window.innerHeight - 90;
 		if(window.document.getElementById("global-container")){
@@ -70,9 +78,38 @@ export default {
 			tmpHeight = tmpHeight - 56;
 		}
 		return tmpHeight;
-	}
-	},
-	
+		},
+		// Check to see if the sbcExecutable is running - if not close the DWC side of the plugin
+		checkExecutable(){
+		let self = this;
+		setInterval(function() {self.checkRunning();},5000); // Check every 5 seconds
+		},
+		checkRunning(){
+		let self = this;
+		if (self.isrunning()){
+			return;
+		}
+		self.stopthePlugin();
+		},
+		async stopthePlugin(){
+			// console.log('Stopping the plugin');
+			store.dispatch("machine/unloadDwcPlugin", pluginName); // Unload the plugin
+		},
+		// sbc Executable is running if pid > 0
+		isrunning() {
+		const allplugins = store.state.machine.model.plugins;
+		for (let [key, value] of allplugins) {
+    		//console.log(key, value);
+			if (key == pluginName){
+				//console.log('Pid = ' + value.pid);
+				if (value.pid > 0){
+					return true
+				};
+				return false;
+			};
+		};
+		}
+	},	
 	// use computed instead of methods cuz we only want it to run once
 	computed:{
 		systemDirectory() {
@@ -80,21 +117,13 @@ export default {
 		},
 		showBottomNavigation() {
 		return this.$vuetify.breakpoint.mobile && !this.$vuetify.breakpoint.xsOnly && this.$store.state.settings.bottomNavigation;
-		},
- 
-		theurl() {
-			return this.myurl;
-		},
-		theheight() {
-			return this.tmpHeight;
 		}
 	},
 	mounted() {
 		this.loadSettingsFromFile();
 		this.getAvailScreenHeight();
-	},
- 
- 
-}	
+		this.checkExecutable();
+	} 
+}
 </script>
  
